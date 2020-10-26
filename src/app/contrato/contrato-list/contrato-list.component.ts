77e987ca126f9b65179d79c4956c90842ae67e36
Sara Plazas <s.plazas@uniandes.edu.co>
2020-10-26 17:51:50 -0500

    Ejemplo clase

---
+++ src/app/contrato/contrato-list/contrato-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Contrato } from '../contrato';
+import { ContratoServiceService} from '../contrato-service.service';
+
+@Component({
+  selector: 'app-contrato-list',
+  templateUrl: './contrato-list.component.html',
+  styleUrls: ['./contrato-list.component.css']
+})
+export class ContratoListComponent implements OnInit {
+
+  constructor(private service: ContratoServiceService) { }
+  contratos: Array<Contrato>;
+  getContratos(): any{
+    return this.service.getContratos().subscribe(c => this.contratos = c);
+  }
+  ngOnInit() {
+    this.getContratos();
+  }
+
+}
