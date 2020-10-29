ff6106f78f59a0f3abd71dce053f444e9ba76f3a
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2020-10-29 17:46:21 -0500

    Se arregló la url del api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ const rootApi = 'api';
 
 export const environment = {
   production: false,
-  baseUrl: 'http://${host}:${port}/${appName}/${rootApi}/'
+  baseUrl: `http://${host}:${port}/${appName}/${rootApi}/`
 };
 
 /*
