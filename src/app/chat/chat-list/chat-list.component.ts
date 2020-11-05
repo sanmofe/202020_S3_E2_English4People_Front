a678b43cf94cb0f034409532da9936434258cc28
Juan David Becerra Romero <jd.becerra@uniandes.edu.co>
2020-11-05 15:08:06 -0500

    ...

--- src/app/chat/chat-list/chat-list.component.ts
+++ src/app/chat/chat-list/chat-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chat } from '../chat';
 import { ChatServiceService} from '../chat-service.service';
+import { ChatDetail} from '../chatDetail';
 
 @Component({
   selector: 'app-chat-lista',
@@ -11,6 +12,14 @@ export class ChatListComponent implements OnInit {
 
   constructor(private service: ChatServiceService) { }
   chats: Array<Chat>;
+  selectedChat: ChatDetail;
+  selected = false;
+
+  onSelected(c: ChatDetail):void{
+    this.selected = true;
+    this.selectedChat = c;
+  }
+
   getChats(): any{
     return this.service.getChats().subscribe(c => this.chats = c);
   }
--- src/app/estudiante/estudiante-list/estudiante-list.component.ts
+++ src/app/estudiante/estudiante-list/estudiante-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Estudiante } from '../estudiante';
 import { EstudianteServiceService} from '../estudiante-service.service';
+import {EstudianteDetail} from '../estudianteDetail';
 
 @Component({
   selector: 'app-estudiante-list',
@@ -11,6 +12,14 @@ export class EstudianteListComponent implements OnInit {
 
   constructor(private service: EstudianteServiceService) { }
   estudiantes: Array<Estudiante>;
+  selectedEstudiante: EstudianteDetail;
+  selected = false;
+
+  onSelected(e: EstudianteDetail):void{
+    this.selected = true;
+    this.selectedEstudiante = e;
+  }
+
   getEstudiantes(): any{
     return this.service.getEstudiantes().subscribe(c => this.estudiantes = c);
   }
