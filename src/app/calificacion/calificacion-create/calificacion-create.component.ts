6b706f7e7eb8c558e1841b450d49a9cc53fea26a
sanmofe <s.moraf@uniandes.edu.co>
2020-11-30 12:11:35 -0500

    Calificacion está en la interfaz, intento de hacer que parezca una app real

---
+++ src/app/calificacion/calificacion-create/calificacion-create.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+@Component({
+  selector: 'app-calificacion-create',
+  templateUrl: './calificacion-create.component.html',
+  styleUrls: ['./calificacion-create.component.css']
+})
+export class CalificacionCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/calificacion/calificacion.module.ts
+++ src/app/calificacion/calificacion.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalificacionListaComponent } from './calificacion-lista/calificacion-lista.component'
 import { CalificacionDetailComponent } from './calificacion-detail/calificacion-detail.component'
+import { CalificacionCreateComponent } from './calificacion-create/calificacion-create.component'
+import { ReactiveFormsModule } from '@angular/forms'
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, ReactiveFormsModule
   ],
-  declarations: [CalificacionListaComponent, CalificacionDetailComponent],
-  exports: [CalificacionListaComponent]
+  declarations: [CalificacionListaComponent, CalificacionDetailComponent, CalificacionCreateComponent],
+  exports: [CalificacionListaComponent, CalificacionCreateComponent]
 })
 export class CalificacionModule { }
