02d310e4c469584742b902e03f579db38e295231
Juan David Becerra Romero <jd.becerra@uniandes.edu.co>
2020-12-02 21:40:48 -0500

    ...

---
+++ src/app/estudiante/estudiante-create/estudiante-create.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
+import { Profesor } from '../profesor';
+import { ProfesorServiceService} from '../profesor-service.service';
+import { ProfesorDetail} from '../profesorDetail';
+
+
+@Component({
+  selector: 'app-profesor-create',
+  templateUrl: './profesor-create.component.html',
+  styleUrls: ['./profesor-create.component.css']
+})
+export class ProfesorCreateComponent implements OnInit {
+
+  profesorForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private toastrService: ToastrService, private profesorService: ProfesorServiceService) { }
+
+  cancelCreation(): void {
+    this.toastrService.warning('El profesor no fue creado', 'Creacion profesor');
+    this.profesorForm.reset();
+  }
+
+  ngOnInit() {
+    this.profesorForm = this.formBuilder.group({
+      nombre: ["", [Validators.required, Validators.minLength(2)]],
+      identificacion: ["", [Validators.required, Validators.maxLength(12)]],
+      login: ["", [Validators.required, Validators.minLength(2)]],
+      correo: ["", [Validators.required, Validators.email]],
+      contrasena: ["",[Validators.required,]],
+      informacionAcademica:["",[Validators.required]],
+      canalYoutube:[""]
+    });
+  }
+
+  createProfesor(profesor: ProfesorDetail) {
+    this.profesorService.createProfesor(profesor)
+      .subscribe(profesor => {
+        this.toastrService.success('El profesor fue creado');
+        this.profesorForm.reset()
+      }, err => {
+        this.toastrService.error(err, 'Error');
+      });
+      alert("El profesor fue creado satisfactoriamente!");
+  }
+
+}
